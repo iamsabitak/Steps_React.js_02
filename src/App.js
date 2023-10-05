fb2623f93c5ba4dc271191c1a25c9d0d757392ed
sabita <sabitakhadka091@gmail.com>
2023-10-05 17:11:50 +0545

    Css implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+ 
 export default function App() {
-  return <div className="steps">
-  <div className="numbers">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-  </div>
-  </div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div className="active">2</div>
+        <div className="active">3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button style={{backgroundColor:"#7950f2",color:"#fff"}}>Previous</button>
+        <button style={{backgroundColor:"#7950f2",color:"#fff"}}>Next</button>
+      </div>
+    </div>
+  );
 }
