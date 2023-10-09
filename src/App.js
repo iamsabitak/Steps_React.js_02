fd0bf3125845b982a59dcf35dc1e909552b07815
sabita <sabitakhadka091@gmail.com>
2023-10-09 16:34:01 +0545

    change some CSs

--- src/App.js
+++ src/App.js
@@ -7,40 +7,61 @@ const messages = [
 ];
 
 export default function App() {
-  // const step = 1;
-  const [step, setStep] = useState(1);
-  const handleOnNext = () => {
-    setStep(step + 1);
-  };
-  const handleOnPrevious = () => {
-    setStep(step - 1);
-  };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step{step}:{messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-        disabled={step <= 1}
-          onClick={ handleOnPrevious}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Previous
-        </button>
-        <button
-        disabled={step >= 3}
-          onClick={handleOnNext}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+    <Steps />
+      <Steps />
+      <Steps />
+      <Steps />
     </div>
   );
+
+  function Steps() {
+    const [step, setStep] = useState(1);
+    const [isOpen, setisOpen] = useState(true);
+    const handleOnNext = () => {
+      setStep((s) => s + 1);
+    };
+    const handleOnPrevious = () => {
+      setStep((s) => s - 1);
+    };
+    const onHandleClose = () => {
+      setisOpen((isOpen) => !isOpen);
+    };
+    return (
+      <div>
+        <div className="close" onClick={onHandleClose}>
+          &times;
+        </div>
+        {isOpen && (
+          <div className="steps">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
+            <p className="message">
+              Step{step}:{messages[step - 1]}
+            </p>
+            <div className="buttons">
+              <button
+                disabled={step <= 1}
+                onClick={handleOnPrevious}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                Previous
+              </button>
+              <button
+                disabled={step >= 3}
+                onClick={handleOnNext}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
