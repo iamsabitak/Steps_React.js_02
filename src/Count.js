6ffed01683ee754015a72c39255f2ee44b34e304
sabita <sabitakhadka091@gmail.com>
2023-10-16 14:45:47 +0545

    did some little changes on Count.js comnponent

--- src/Count.js
+++ src/Count.js
@@ -3,18 +3,36 @@ import React, { useState } from "react";
 function Count() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(0);
-  const date = new Date("Oct 10 2023");
+  // const date = new Date("Oct 10 2023");
+  const date = new Date();
   date.setDate(date.getDate() + count);
+
+  const handleClick = () => {
+    setCount(0);
+    setStep(1);
+  };
   return (
     <>
       <div>
-        <button onClick={() => setStep(step - 1)}>-</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* <button onClick={() => setStep(step - 1)}>-</button> */}
         <span>Step:{step}</span>
         <button onClick={() => setStep(step + 1)}>+</button>
       </div>
       <div>
         <button onClick={() => setCount(count - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        {/* <span>Count: {count}</span> */}
         <button onClick={() => setCount(count + step)}>+</button>
       </div>
       <p>
@@ -27,6 +45,11 @@ function Count() {
         </span>
         <span> {date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleClick}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
